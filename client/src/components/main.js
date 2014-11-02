098164c7c6ec11f6ecfcd6e80c6a95c2d1948699
Bryce Neal <brycedneal@gmail.com>
2014-11-01 19:20:15 -0700

    Fix satoshi miscalculation.

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -23,12 +23,12 @@ var Main = React.createClass({
     render: function() {
         return (
             <div className="row">
-                <a href="https://github.com/prettymuchbryce/chainreaction"><img className="github-banner" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"/></a>
+                <a href="https://github.com/prettymuchbryce/chainreaction"><img className="github-banner" src="http://aral.github.com/fork-me-on-github-retina-ribbons/right-graphite@2x.png" alt="Fork me on GitHub"/></a>
                 <div className="col-md-12 text-center">
                     <h3>New Transactions on the BTC Network</h3>
-                    <button type="button" className="btn btn-success">{'<100 BTC'}</button>
-                    <button type="button" className="btn btn-warning">{'>=100 BTC'}</button>
-                    <button type="button" className="btn btn-danger">{'>=1000 BTC'}</button>
+                    <button type="button" className="btn btn-success">{'<50 BTC'}</button>
+                    <button type="button" className="btn btn-warning">{'>=50 BTC'}</button>
+                    <button type="button" className="btn btn-danger">{'>=500 BTC'}</button>
                     <br/>
                     <small>Click any transaction below to see more info on blockchain.info</small>
                     <TransactionList ref="list"/>
--- client/src/components/transaction-item.js
+++ client/src/components/transaction-item.js
@@ -13,9 +13,9 @@ var TransactionItem = React.createClass({
         var inputs = inputs.join(", ");
         var outputs = outputs.join(", ");
 
-        if (this.props.amount/ 100000 > 1000) {
+        if (this.props.amount/ 100000000 > 500) {
             this.props.panelType = "btn btn-danger";
-        } else if (this.props.amount/ 100000 > 100) {
+        } else if (this.props.amount/ 100000000 > 50) {
             this.props.panelType = "btn btn-warning";
         } else {
             this.props.panelType = "btn btn-success"
@@ -23,10 +23,12 @@ var TransactionItem = React.createClass({
 
         var link = "https://blockchain.info/tx/" + this.props.hash
 
+        var seconds = 0;
+
         return (
             <a href={link}>
                 <button type="button" className={this.props.panelType}>
-                    <img className="btc-icon" data-toggle="tooltip" data-placement="left" title="Transaction Amount" src="/building/chainreaction/img/btc-icon.jpg"/>: {this.props.amount / 100000} <img className="miner-icon" src="/building/chainreaction/img/miner-icon.png" data-toggle="tooltip" data-placement="left" title="Miners Fee"/>: {this.props.fees / 100000}
+                    <img className="btc-icon" data-toggle="tooltip" data-placement="left" title="Transaction Amount" src="/building/chainreaction/img/btc-icon.jpg"/>: {this.props.amount / 100000000} <img className="miner-icon" src="/building/chainreaction/img/miner-icon.png" data-toggle="tooltip" data-placement="left" title="Miners Fee"/>: {this.props.fees / 100000000}
                 </button>
             </a>
         )
