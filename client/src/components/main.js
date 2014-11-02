48ec7160ac275df1bc16cc947a1dad566a3e6b20
Bryce Neal <brycedneal@gmail.com>
2014-11-01 20:41:42 -0700

    Various changes

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -25,12 +25,17 @@ var Main = React.createClass({
             <div className="row">
                 <a href="https://github.com/prettymuchbryce/chainreaction"><img className="github-banner" src="http://aral.github.com/fork-me-on-github-retina-ribbons/right-graphite@2x.png" alt="Fork me on GitHub"/></a>
                 <div className="col-md-12 text-center">
-                    <h3>New Transactions on the BTC Network</h3>
-                    <button type="button" className="btn btn-success">{'<50 BTC'}</button>
-                    <button type="button" className="btn btn-warning">{'>=50 BTC'}</button>
-                    <button type="button" className="btn btn-danger">{'>=500 BTC'}</button>
-                    <br/>
-                    <small>Click any transaction below to see more info on blockchain.info</small>
+                    <h3>Realtime transactions on the BTC Network</h3>
+                    <div className="panel panel-default">
+                        <button type="button" className="btn btn-success">{'<50 BTC'}</button>
+                        <button type="button" className="btn btn-warning">{'>=50 BTC'}</button>
+                        <button type="button" className="btn btn-danger">{'>=500 BTC'}</button>
+                        <br/><br/>
+                        <img className="btc-icon" data-toggle="tooltip" data-placement="left" title="Transaction Amount" src="/building/chainreaction/img/btc-icon.png"/>: Amount &nbsp;&nbsp;&nbsp;<img className="miner-icon" src="/building/chainreaction/img/miner-icon.png" data-toggle="tooltip" data-placement="left" title="Miners Fee"/>: Miners Fee
+                        <br/>
+
+                        <small>Click any transaction below to see more info on blockchain.info</small>
+                    </div>
                     <TransactionList ref="list"/>
                 </div>
             </div>
